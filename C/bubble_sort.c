cc87d2eba54428c506871b12bd62d196c9f4553a
smhassanerfani <serfani@email.sc.edu>
2022-02-22 15:23:21 -0500

    Added bubble_sort.c

---
+++ C/bubble_sort.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void print_array(int lst[], int num);
+void bubble_sort(int lst[], int num);
+void swap(int lst[], int i, int j);
+
+int main()
+{
+	int p[] = {2, 5, 3, 6, 4, 4, 0};
+	int n = sizeof(p) / sizeof(p[0]);
+
+	print_array(p, n);
+
+	bubble_sort(p, n);
+	print_array(p, n);
+
+	return 0;
+}
+
+void print_array(int lst[], int num)
+{
+	printf("List of array elements:\n");
+	for(int i=0; i<num; i++)
+	{
+		printf("%d", lst[i]);
+		if(i<num-1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
+
+void bubble_sort(int lst[], int num)
+{
+	for(int i=0; i<num; i++)
+	{
+		int swapped = 0;
+		for(int j=0; j<num-1; j++)
+		{
+			if(lst[j] > lst[j+1])
+			{
+				swap(lst, i, j);
+				swapped = 1;
+			}
+		}
+		if(!swapped) break;
+	}
+}
+
+void swap(int lst[], int i, int j)
+{
+	int temp = lst[j];
+	lst[j] = lst[j+1];
+	lst[j+1] = temp;
+}
