d14ea953053fc125e1f24339f114ca6a5ace0ad5
smhassanerfani <serfani@email.sc.edu>
2022-02-23 15:35:18 -0500

    Added pointers.c

---
+++ C/pointers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void func1(int *aPtr);
+
+int main()
+{
+	int a;
+	printf("Please enter your number:");
+	scanf("%d", &a);
+	// printf("\n");
+	
+	printf("Your number is: %d\n", a);
+
+	func1(&a);
+	printf("Your number after calling func1: %d\n", a);
+
+	return 0;
+}
+
+void func1(int *aPtr) // Call by Reference
+{
+	*aPtr *= 2;
+	printf("Your number inside the func1 is: %d\n", *aPtr);
+}
