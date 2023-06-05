de0381226aedb9fc4a0afbabae919f8db11ec43a
smhassanerfani <serfani@email.sc.edu>
2023-06-05 12:28:45 -0400

    Added Maps dataloader

--- GANs/dataloader.py
+++ GANs/dataloader.py
@@ -1,76 +1,66 @@
+import os
+import numpy as np
+import torch
 from torch.utils.data import Dataset
 from PIL import Image
-import os
+from torchvision.transforms import Compose, ToTensor, Normalize, ColorJitter, RandomHorizontalFlip, ToPILImage
 
 
-class ToHSV:
-    def __call__(self, sample):
-        # inputs = sample
-        return sample.convert('HSV')
+class Maps(Dataset):
 
-
-class ATeX(Dataset):
-    def __init__(self, rootdir="./dataset/atex", split="train", as_gray=False, transform=None):
-        super(ATeX, self).__init__()
-        self.rootdir = rootdir
+    def __init__(self, root, split, joint_transform=None):
+        super(Maps, self).__init__()
+        self.root = root
         self.split = split
-        self.transform = transform
-        self.images_base = os.path.join(self.rootdir, self.split)
-        self.items_list, self.classes = self._get_images_list()
-        self.as_gray = as_gray
+        self.images_base = os.path.join(self.root, self.split)
+        # self.masks_base = os.path.join(self.root, "masks", self.split)
+        self.items_list = self._get_images_list()
+
+        self.joint_transform = joint_transform
+
+        mean_std = ([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+        self.image_transforms = Compose([ToTensor(), ColorJitter(), Normalize(*mean_std)])
+        self.mask_transforms = Compose([ToTensor(), Normalize(*mean_std)])
 
     def _get_images_list(self):
-        items_list = list()
-        classes = list()
-        counter = 0
+        items_list = []
         for root, dirs, files in os.walk(self.images_base, topdown=True):
-            if counter == 0:
-                classes = dirs
 
-            for file in files:
-                if file.endswith(".jpg"):
-                    items_list.append({
-                        "image": os.path.join(root, file),
-                        "label": counter - 1,
-                        "class_name": classes[counter - 1]
-                    })
-            counter += 1
-        return items_list, classes
-
-    # @property
-    # def classes(self):
-    #     classes = list()
-    #     with os.scandir(self.images_base) as it:
-    #         for entry in it:
-    #             if entry.is_dir():
-    #                 classes.append(entry.name)
-    #     return classes
+            for name in files:
+                if name.endswith(".jpg"):
+                    img_file = os.path.join(root, name)
+                    items_list.append(img_file)
+        return items_list
 
     def __getitem__(self, index):
-        image_path = self.items_list[index]["image"]
-        label = self.items_list[index]["label"]
-        class_name = self.items_list[index]["class_name"]
+        image_path = self.items_list[index]
+        jpg = np.array(Image.open(image_path))
+        image = jpg[:, :600, :]
+        mask = jpg[:, 600:, :]
 
-        if self.as_gray:
-            image = Image.open(image_path).convert('L')
 
-        else:
-            image = Image.open(image_path).convert('RGB')
+        if self.joint_transform:
+            image, mask = self.joint_transform(Image.fromarray(image), Image.fromarray(mask))
+        image = self.image_transforms(image)
+        mask = self.mask_transforms(mask)
 
-        if self.transform:
-            image = self.transform(image)
 
-        return image, label, class_name
+        return image, mask
 
     def __len__(self):
         return len(self.items_list)
 
 
 def main():
-    dataset = ATeX()
-    data_iter = iter(dataset)
-    items = next(data_iter)
-    print(items)
+    dataset = Maps("./dataset/maps", "val")
+    print(len(dataset))
+    dataiter = iter(dataset)
+    image, mask = next(dataiter)
+    print(image.shape, mask.shape)
+
+    # transform = ToPILImage()
+    # mask = transform(mask)
+    # mask.show()
 
 if __name__ == '__main__':
     main()
