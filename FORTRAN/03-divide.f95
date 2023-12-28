8ca18ea0550e2625f2b3fcd6e9d04f0925b660f0
smhassanerfani <serfani@email.sc.edu>
2023-12-28 11:29:03 -0500

    feat(FORTRAN): add `03-divide.f95

---
+++ FORTRAN/03-divide.f95
@@ -0,0 +1,12 @@
+program divide
+    implicit none
+
+    ! Variable Declaration
+    real :: x, y
+
+    x = 1.0
+    y = x / 3.0
+
+    print *, 'Results of 1 divided by 3 is: ', y
+
+end program divide
